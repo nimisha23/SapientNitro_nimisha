71d9cd3555379c69504074a5f51d1106d8d698f1
unknown <nsri16@deltahiti46611.sapient.com>
2015-08-18 16:51:24 +0530

    variables added

--- Demo1/src/MainPack/MainApp.java
+++ Demo1/src/MainPack/MainApp.java
@@ -3,8 +3,11 @@ package MainPack;
 public class MainApp {
 
 	public static void main(String[] args) {
+		
+		int n, u=6;
 		// TODO Auto-generated method stub
 System.out.println("hello sapient");
+
 	}
 
 }
